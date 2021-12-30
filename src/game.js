4cf4d4dd4d666ec6c4768e46be6a6ecc588ec56d
arleen <arleenpandher11@gmail.com>
2021-12-30 18:02:55 -0500

    Direction's Display

--- src/game.js
+++ src/game.js
@@ -1,8 +1,10 @@
-const compdirection = require("./compdirection.js")
+const Compdirection = require("./compdirection.js")
 
 const CONSTANTS = {
     DIRECTIONS: ["North", "South", "East", "West"],
-    NUM_DIRS: 5
+    NUM_DIRS: 5,
+    DIM_X: 600,
+    DIM_Y: 600,
 };
 
 function Game() {
@@ -11,12 +13,26 @@ function Game() {
 }
 
 Game.prototype.addDirections = function() {
-    for (i = 0; i < CONSTANTS[NUM_DIRS]; i++) {
-        this.directions.push(new Compdirection(randomdirectiongenerator()))
+    for (i = 0; i < CONSTANTS.NUM_DIRS; i++) {
+        this.directions.push(new Compdirection(this.randomdirectiongenerator()))
     }
 }
 
 Game.prototype.randomdirectiongenerator = function() {
     let randomnum = Math.floor(4 * Math.random())
-    return CONSTANTS[DIRECTIONS][randomnum]
+    return CONSTANTS.DIRECTIONS[randomnum]
 }
+
+Game.prototype.draw = function(ctx) {
+    for (i = 0; i < CONSTANTS.NUM_DIRS; i++) {
+        ctx.clearRect(0, 0, CONSTANTS.DIM_X, CONSTANTS.DIM_Y);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, CONSTANTS.DIM_X,  CONSTANTS.DIM_Y);
+        this.directions[i].display(ctx)
+        i++
+    }
+}
+
+
+
+module.exports = Game
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const Compdirection = require("./compdirection.js")
+const Game = require("./game.js")
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
     console.log("Webpack is working!")
 
-    const text = new Compdirection("North")
-    text.display(ctx)
+    // const text = new Compdirection("North")
+    // text.display(ctx)
+
+    const game = new Game()
+    console.log(game.directions)
+    game.draw(ctx)
 
 });
